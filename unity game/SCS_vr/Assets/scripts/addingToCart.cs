f23106ea073979bb2e6ef55fb3a2b26577ecafd7
DiptiGupte <gupte.d50@gmail.com>
2019-04-24 17:45:11 -0700

    detecting which ingredient is put in cart

--- unity game/SCS_vr/Assets/scripts/addingToCart.cs
+++ unity game/SCS_vr/Assets/scripts/addingToCart.cs
@@ -2,17 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class addingToCart : MonoBehaviour {
-
+public class addingToCart : MonoBehaviour
+{
+    bool one = false;
+    bool two = false;
+    bool three = false;
     void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.tag == "nonvegan")
+        if (col.gameObject.tag == "ingredient1")
         {
-            Debug.Log("nonvegan collision detected");
+            if (one == false)
+            {
+                one = true;
+                if (col.gameObject.name == "nonvegan1")
+                {
+                    Debug.Log("nonvegan1 collision detected");
+                    //change points
+                }
+                else if (col.gameObject.name == "vegan1")
+                {
+                    Debug.Log("vegan1 collision detected");
+                    //change points
+                }
+            }
+            else
+            {
+                Debug.Log("ingredient1 already picked");
+                //reject object
+            }
+            
         }
-        else if (col.gameObject.tag == "vegan")
+        else if (col.gameObject.tag == "ingredient2")
         {
-            Debug.Log("vegan collision detected");
+            if (two == false)
+            {
+                two = true;
+                if (col.gameObject.name == "nonvegan2")
+                {
+                    Debug.Log("nonvegan2 collision detected");
+                    //change points
+                }
+                else if (col.gameObject.name == "vegan2")
+                {
+                    Debug.Log("vegan2 collision detected");
+                    //change points
+                }
+            }
+            else
+            {
+                Debug.Log("ingredient2 already picked");
+                //reject object
+            }
+
+        }
+        else if (col.gameObject.tag == "ingredient3")
+        {
+            if (three == false)
+            {
+                three = true;
+                if (col.gameObject.name == "nonvegan3")
+                {
+                    Debug.Log("nonvegan3 collision detected");
+                    //change points
+                }
+                else if (col.gameObject.name == "vegan3")
+                {
+                    Debug.Log("vegan3 collision detected");
+                    //change points
+                }
+            }
+            else
+            {
+                Debug.Log("ingredient3 already picked");
+                //reject object
+            }
+
         }
     }
 }
