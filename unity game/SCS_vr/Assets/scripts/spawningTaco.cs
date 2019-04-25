9e20055a1e2f533fd3cedd54a7a3e686cc7c45a6
DiptiGupte <gupte.d50@gmail.com>
2019-04-24 18:11:47 -0700

    created spawningTaco script

---
+++ unity game/SCS_vr/Assets/scripts/spawningTaco.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawningTaco : MonoBehaviour
+{
+    public Transform Spawnpoint;
+    public GameObject Taco;
+    void OnTriggerEnter() => Instantiate(Taco, Spawnpint.position, Spawnpoint.rotation);
+}
