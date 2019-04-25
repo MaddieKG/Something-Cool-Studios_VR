13bf59ffc005343266628ffc57bcd12198088184
Andrea Horvath <ahorvath@scu.edu>
2019-04-25 11:52:37 -0700

    Created translator script that has placeholder text

---
+++ unity game/SCS_vr/Assets/Scripts/TranslationControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TranslationControl : MonoBehaviour {
+
+    public Text message;
+
+	void Start () {
+        message.text = "Waiting for input...";
+	}
+	
+    //Call when customer is in front of window
+    private void customerTalks(string m)
+    {
+        message.text = m;
+    }
+}
