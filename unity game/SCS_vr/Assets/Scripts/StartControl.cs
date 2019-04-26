b08ee563166ef80edb454334d5d19cefc61853b2
Andrea Horvath <ahorvath@scu.edu>
2019-04-25 18:44:15 -0700

    added StartControl script to change start/stop when button is pressed

---
+++ unity game/SCS_vr/Assets/Scripts/StartControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartControl : MonoBehaviour {
+
+    public bool start;
+	//object this is attached to needs a RigidBody
+
+	void Start () {
+        start = false;
+	}
+
+	
+	void Update () {
+	    //may need for future use
+	}
+
+    //Is button colliding with player hand?
+    //Returns bool
+    private bool IsHand(Collider other)
+    {
+        if (other.transform.parent && other.transform.parent.parent && other.transform.parent.parent.GetComponent<HandModel>())
+            return true;
+        else
+            return false;
+    }
+
+    //Checks whenever something collides with object if it is hand
+    //Changes start variable
+    void OnTriggerEnter(Collider other)
+    {
+        if (IsHand(other))
+        {
+            Debug.Log("Yay! A hand collided!");
+            start = !start;
+        }
+    }
+}
