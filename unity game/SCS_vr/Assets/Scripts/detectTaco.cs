f5b1f73a3ba343350183f93fc3396a15804ecfb0
DiptiGupte <gupte.d50@gmail.com>
2019-04-28 22:55:29 -0700

    Detect and destroy taco when it hits counter

---
+++ unity game/SCS_vr/Assets/Scripts/detectTaco.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detectTaco : MonoBehaviour
+{
+	//IEnumerator
+	IEnumerator OnCollisionEnter(Collision col)
+	{
+			if (col.gameObject.name == "taco(Clone)")
+			{
+				yield return new WaitForSeconds(3);
+				Destroy(col.gameObject);
+				//check customer preference and update points
+			}
+	}
+}
