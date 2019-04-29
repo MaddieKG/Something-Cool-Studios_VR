ab85f9a46724a9d843b5410137eacf7e01782d6d
Cole Dennis <coleedennis@gmail.com>
2019-04-29 16:36:49 -0700

    working on spawning customers

---
+++ unity game/SCS_vr/Assets/customerInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class customerInfo : MonoBehaviour {
+    public all_cust_opts info; // Reference to the customer's preferences of greens and meat and their name
+    public GameObject custObj; //The customer prefab to be spawned.
+    public float spawnTime = 3f;            // How long between each spawn.
+
+
+}
