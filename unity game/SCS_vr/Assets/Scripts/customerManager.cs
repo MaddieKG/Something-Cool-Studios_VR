1edb99d1f876533c9b834e8499e740f770658cbe
Dipti Gupte <gupte.d50@gmail.com>
2019-04-29 20:35:16 -0700

    customer spawning with stats

---
+++ unity game/SCS_vr/Assets/Scripts/customerManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class customerManager : MonoBehaviour {
+    //array of game objects
+    public GameObject[] allCust;
+    private GameObject current;
+    public GameObject pos;
+
+    void SpawnCustomer()
+    {
+        int i = Random.Range(0, allCust.Length);
+        current = allCust[i];
+        Instantiate(current, pos.transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            SpawnCustomer();
+            Debug.Log(current.name);
+        }
+	}
+}
