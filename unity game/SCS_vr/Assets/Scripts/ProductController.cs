e1c8983a39ea02bfbb6a0ec954af621c3221b7c4
Andrea Horvath <ahorvath@scu.edu>
2019-04-29 19:23:55 -0700

    added feature to productController script that makes object unabled to be grabbed at button - needs more work

--- unity game/SCS_vr/Assets/Scripts/ProductController.cs
+++ unity game/SCS_vr/Assets/Scripts/ProductController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class ProductController : MonoBehaviour {
     public int money, green;
-
+    private GameObject startButton;
+    //private RigidBody rb;
 
     // Use this for initialization
     void Start() {
-        
+        //rb = gameObject.GetComponent<RigidBody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        startButton = GameObject.Find("StartButton");//need a StartButton object
+        StartControl startScript = startButton.GetComponent<StartControl>();
+        if (startScript.start == true)
+        {
+            Destroy(gameObject.GetComponent<Rigidbody>());
+            //rb.detectCollisions = false;
+        }
+        else
+        {
+            gameObject.AddComponent<Rigidbody>();
+            //rb.detectCollisions = true;
+        }
+    }
 }
