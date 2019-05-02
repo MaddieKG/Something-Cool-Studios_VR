a26cde8d839887a549a66872aa0028516c1637c4
Cole Dennis <coleedennis@gmail.com>
2019-05-02 11:05:13 -0700

    saved animation script

--- unity game/SCS_vr/Assets/Animations/Animation_on_Key.cs
+++ unity game/SCS_vr/Assets/Animations/Animation_on_Key.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Animation_on_Key : MonoBehaviour {
+public class Cole_Animation_Test_Script : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Animator anim;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    public void walk()
+    {
+        anim.SetBool("TestBool", true);
+        //anim.SetBool("TestBool", false);
+        Debug.Log("walk");
+    }
 }
