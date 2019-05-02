a590454b34d28b42b68e7655b2eeaafb06068457
Andrea Horvath <ahorvath@scu.edu>
2019-05-02 16:40:45 -0700

    added script

---
+++ unity game/SCS_vr/Assets/Scripts/CustomerData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Customer Data", menuName = "Customer Data", order = 51)]
+public class CustomerData : ScriptableObject {
+    public int green, meat;
+    public string name;
+    //[SerializeField] private Animator anim = GetComponent<Animator>;
+
+    public int Green
+    {
+        get { return green; }
+    }
+
+    public int Meat
+    {
+        get { return meat; }
+    }
+
+    public string Name
+    {
+        get { return name; }
+    }
+}
