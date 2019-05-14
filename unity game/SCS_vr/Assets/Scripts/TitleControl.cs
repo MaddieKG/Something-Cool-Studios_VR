bb68b9026bd39ceb35c30e8b5daa9f4812e04f4b
Andrea Horvath <ahorvath@scu.edu>
2019-05-13 20:37:27 -0700

    added tutorial UI

--- unity game/SCS_vr/Assets/Scripts/TitleControl.cs
+++ unity game/SCS_vr/Assets/Scripts/TitleControl.cs
@@ -7,7 +7,8 @@ public class TitleControl : MonoBehaviour {
     [SerializeField]
     private StartData startData;
     private GameObject titleCanvas;
-    private GameObject pointsDisplay, costDisplay, newsDisplay, translator;
+    private GameObject pointsDisplay, costDisplay, newsDisplay, translator, screen;
+    private bool tutorialStart;
 
 	// Use this for initialization
 	void Start () {
@@ -17,8 +18,10 @@ public class TitleControl : MonoBehaviour {
         costDisplay = GameObject.Find("CostDisplay");
         newsDisplay = GameObject.Find("NewsDisplay");
         translator = GameObject.Find("Translator");
+        screen = GameObject.Find("titlescreen");
         
         titleCanvas.SetActive(true);
+        screen.SetActive(true);
         pointsDisplay.SetActive(false);
         costDisplay.SetActive(false);
         newsDisplay.SetActive(false);
@@ -35,11 +38,15 @@ public class TitleControl : MonoBehaviour {
         if (col.gameObject.name == "RightHandAnchor" || col.gameObject.name == "LeftHandAnchor")
         {
             gameObject.SetActive(false);
+            screen.SetActive(false);
             titleCanvas.SetActive(false);
             pointsDisplay.SetActive(true);
             costDisplay.SetActive(true);
             newsDisplay.SetActive(true);
             translator.SetActive(true);
+            tutorialStart = true;
         }
     }
+
+
 }
