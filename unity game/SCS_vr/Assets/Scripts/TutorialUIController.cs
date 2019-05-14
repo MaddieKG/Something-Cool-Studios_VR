ca30d19e09351d8912cc54b03412efaa0a90bb56
Andrea Horvath <ahorvath@scu.edu>
2019-05-14 09:47:18 -0700

    script added

---
+++ unity game/SCS_vr/Assets/Scripts/TutorialUIController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialUIController : MonoBehaviour
+{
+
+    public Text newsText;
+    public Text costText, remainingMonText;
+    public Text moneyText, popText, greenText;
+    public Text message;
+
+    [SerializeField]
+    private PointsData pointsData;
+    private bool isCostShowing;
+    private GameObject costDisplay, startButton, pointsController;
+    private float monRemaining, cost;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Allows for initialization
+        startButton = GameObject.Find("StartButton");//need a StartButton object
+        StartControl startScript = startButton.GetComponent<StartControl>();
+        startScript.start = false;
+        PointsController pointsScript = GameObject.Find("PointsController").GetComponent<PointsController>();
+
+        //cost display stuff
+        costDisplay = GameObject.Find("CostDisplay");
+        //costDisplay.SetActive(true);
+        monRemaining = pointsScript.pointsData.Money;
+        cost = 0;
+
+        //For translator
+        message.text = "Waiting for input...";
+
+        //sets all text
+        setObjective();
+        setCostText(cost);
+        setPointText();
+    }
+
+    private void setObjective()
+    {
+        newsText.text = "Sell some tacos!";
+    }
+
+    //uses money to estimate total cost
+    public void setCost(float cost)
+    {
+        monRemaining -= cost;
+        setCostText(cost);
+    }
+
+    //Hides cost UI whenever buying is not active
+    public void hideCost(bool start)
+    {
+        if (start == false)
+        {
+            costDisplay.SetActive(true);
+        }
+        else
+        {
+            costDisplay.SetActive(false);
+        }
+    }
+
+    //Sets text in cost display UI to correct values
+    public void setCostText(float cost)
+    {
+        PointsController pointsScript = GameObject.Find("PointsController").GetComponent<PointsController>();
+        costText.text = "Total cost: " + cost.ToString("0.##");
+        monRemaining = pointsScript.pointsData.money - cost;
+        remainingMonText.text = "Remaining Money: " + monRemaining.ToString();
+    }
+
+    //use to update whenever changes are made → make global
+    public void setPointText()
+    {
+        PointsController pointsScript = GameObject.Find("PointsController").GetComponent<PointsController>();
+        moneyText.text = "Money: ";// + pointsScript.pointsData.money.ToString();
+        greenText.text = "Green: ";// + pointsScript.pointsData.green.ToString();
+        popText.text = "Popularity: ";// + pointsScript.pointsData.popularity.ToString();
+    }
+
+    public void updateTranslator(string m)
+    {
+        message.text = m;
+    }
+
+}
