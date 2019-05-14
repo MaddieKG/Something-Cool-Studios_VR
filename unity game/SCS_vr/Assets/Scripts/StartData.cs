b53d14addf1554e033f9dc1a866aa0446d1e6729
Andrea Horvath <ahorvath@scu.edu>
2019-05-13 19:47:34 -0700

    implemented start button

---
+++ unity game/SCS_vr/Assets/Scripts/StartData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New StartData", menuName = "Start Data", order = 51)]
+public class StartData : ScriptableObject {
+
+    [SerializeField]
+    public bool start;
+}
---
+++ unity game/SCS_vr/Assets/Scripts/TitleControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleControl : MonoBehaviour {
+
+    [SerializeField]
+    private StartData startData;
+    private GameObject titleCanvas;
+    private GameObject pointsDisplay, costDisplay, newsDisplay, translator;
+
+	// Use this for initialization
+	void Start () {
+        startData.start = false;
+        titleCanvas = GameObject.Find("TitleDisplay");
+        pointsDisplay = GameObject.Find("PointsScreen");
+        costDisplay = GameObject.Find("CostDisplay");
+        newsDisplay = GameObject.Find("NewsDisplay");
+        translator = GameObject.Find("Translator");
+        
+        titleCanvas.SetActive(true);
+        pointsDisplay.SetActive(false);
+        costDisplay.SetActive(false);
+        newsDisplay.SetActive(false);
+        translator.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "RightHandAnchor" || col.gameObject.name == "LeftHandAnchor")
+        {
+            gameObject.SetActive(false);
+            titleCanvas.SetActive(false);
+            pointsDisplay.SetActive(true);
+            costDisplay.SetActive(true);
+            newsDisplay.SetActive(true);
+            translator.SetActive(true);
+        }
+    }
+}
