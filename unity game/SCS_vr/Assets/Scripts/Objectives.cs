55da9eae87f36b09df57585e660a31c0a27b6828
Andrea Horvath <ahorvath@scu.edu>
2019-05-19 16:34:21 -0700

    added game controller script

--- unity game/SCS_vr/Assets/Scripts/Objectives.cs
+++ unity game/SCS_vr/Assets/Scripts/Objectives.cs
@@ -3,29 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Objectives : MonoBehaviour {
-
+    [SerializeField] public ObjectiveData objectiveList;
     private string[] objectives;
     public int rounds;
     private string obj1, obj2, obj3, obj4;
 	// Use this for initialization
 	void Start () {
+        GameController controller = GameObject.Find("GameManager").GetComponent<GameController>();
+
         obj1 = "Shady Sal's lettuce has had an E.coli outbreak. People won't want to buy food made with it. On the bright side, Shady Sal's prices are now lower than ever.";
         obj2 = "Cost of chicken has skyrocketed due to innovation in chicken production. All chicken farms now provide spas to give their chickens comfortable lives.";
         obj3 = "Beef has gone on sale! The ever-popular meat is now at an all-time low price! Buy some before it's gone!";
         obj4 = "Our local farms have raised the price of their lettuce due to water shortage from the current drought.";
-        objectives = new string[] { obj1, obj2, obj3, obj4 };
-        objectives = setObjectives(objectives);
+        objectives = new string[] { obj2, obj1, obj3, obj4 };
+        objectiveList.objectives = setObjectives(objectives, controller.totalRounds);
     }
 	
 	// Update is called once per frame
 	void Update () {
         UIController uicontrol = GameObject.Find("UIController").GetComponent<UIController>();
-        uicontrol.setObjective(objectives[0]);
+        uicontrol.setObjective(objectiveList.objectives[0]);
 	}
 
-    public string[] setObjectives(string[] list)
+    private string[] setObjectives(string[] list, int r)
     {
-        string[] objs = new string[4];
+        string[] objs = new string[r];
         int[] objUsed = new int[] { 0, 0, 0, 0 };
         int val;
         bool used = false;
@@ -46,4 +48,33 @@ public class Objectives : MonoBehaviour {
         }
         return objs;
     }
+
+    private void checkIngredients()
+    {
+        GameController controller = GameObject.Find("GameManager").GetComponent<GameController>();
+        addingToCart cartScript = GameObject.Find("cart").GetComponent<addingToCart>();
+
+        int r = controller.round;
+        if (objectiveList.objectives[r] == obj1 && cartScript.currentLettuce == 0)
+        {
+            //do something
+        } 
+        else if (objectiveList.objectives[r] == obj2)
+        {
+
+        }
+        else if (objectiveList.objectives[r] == obj3)
+        {
+
+        }
+        else if (objectiveList.objectives[r] == obj4)
+        {
+
+        }
+    }
+
+    /*public string[] getObjectives
+    {
+        return { "a"}
+    }*/
 }
---
+++ unity game/SCS_vr/Assets/Scripts/Scriptable Objects/ObjectiveData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New ObjectiveData", menuName = "Objective Data", order = 51)]
+public class ObjectiveData : ScriptableObject {
+
+    public string[] objectives;
+}
