3de2dfea59833e578d7bcbbcc5650c624a48d56e
Andrea Horvath <ahorvath@scu.edu>
2019-05-18 19:22:20 -0700

    ui tracks camera rotation

--- unity game/SCS_vr/Assets/Scripts/GreenFactController.cs
+++ unity game/SCS_vr/Assets/Scripts/GreenFactController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GreenFactController : MonoBehaviour {
     private GameObject infoCanvas;
     private Vector3 displacement = new Vector3(0, 0.1f, 0);
+    public Camera cameraToLookAt;
 
 	// Use this for initialization
 	void Start () {
@@ -15,6 +16,7 @@ public class GreenFactController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         infoCanvas.transform.position = gameObject.transform.position + displacement;
+        infoCanvas.transform.rotation = cameraToLookAt.transform.rotation;
     }
 
     void OnTriggerStay(Collider col)
@@ -22,6 +24,7 @@ public class GreenFactController : MonoBehaviour {
         if (col.gameObject.name == "LeftHandAnchor" || col.gameObject.name == "RightHandAnchor")
         {
             infoCanvas.GetComponent<RectTransform>().anchoredPosition = new Vector3(0.0f, 0.0f);//gameObject.transform.position.x, gameObject.transform.position.y + 10.0f);//gameObject.transform.position + displacement;
+            infoCanvas.transform.rotation = cameraToLookAt.transform.rotation;
             infoCanvas.SetActive(true);
             Debug.Log("beef: " + gameObject.transform.position);
             Debug.Log("canvas: " + infoCanvas.GetComponent<RectTransform>().anchoredPosition);
