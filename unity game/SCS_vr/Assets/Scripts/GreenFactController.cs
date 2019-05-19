29913ed22d855c00a1f5c3b230a2bc6094dba387
Andrea Horvath <ahorvath@scu.edu>
2019-05-18 19:05:15 -0700

    UI canvas appears and moves with object when picked up

---
+++ unity game/SCS_vr/Assets/Scripts/GreenFactController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenFactController : MonoBehaviour {
+    private GameObject infoCanvas;
+    private Vector3 displacement = new Vector3(0, 0.1f, 0);
+
+	// Use this for initialization
+	void Start () {
+        infoCanvas = GameObject.Find("InfoCanvas");
+        infoCanvas.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        infoCanvas.transform.position = gameObject.transform.position + displacement;
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.name == "LeftHandAnchor" || col.gameObject.name == "RightHandAnchor")
+        {
+            infoCanvas.GetComponent<RectTransform>().anchoredPosition = new Vector3(0.0f, 0.0f);//gameObject.transform.position.x, gameObject.transform.position.y + 10.0f);//gameObject.transform.position + displacement;
+            infoCanvas.SetActive(true);
+            Debug.Log("beef: " + gameObject.transform.position);
+            Debug.Log("canvas: " + infoCanvas.GetComponent<RectTransform>().anchoredPosition);
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.name == "LeftHandAnchor" || col.gameObject.name == "RightHandAnchor")
+        {
+            infoCanvas.SetActive(false);
+        }
+    }
+}
