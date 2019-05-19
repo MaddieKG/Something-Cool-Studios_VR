6b68e55d99a67fd2acf11c23996fbf82ae138c86
Andrea Horvath <ahorvath@scu.edu>
2019-05-18 19:51:19 -0700

    each object has its own fact

--- unity game/SCS_vr/Assets/Scripts/GreenFactScript/OrgLettFact.cs
+++ unity game/SCS_vr/Assets/Scripts/GreenFactScript/OrgLettFact.cs
@@ -2,19 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GreenFactController : MonoBehaviour {
+public class OrgLettFact : MonoBehaviour
+{
     private GameObject infoCanvas;
     private Vector3 displacement = new Vector3(0, 0.1f, 0);
     public Camera cameraToLookAt;
 
-	// Use this for initialization
-	void Start () {
-        infoCanvas = GameObject.Find("InfoCanvas");
+    // Use this for initialization
+    void Start()
+    {
+        infoCanvas = GameObject.Find("OrgLettInfo");
         infoCanvas.SetActive(false);
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         infoCanvas.transform.position = gameObject.transform.position + displacement;
         infoCanvas.transform.rotation = cameraToLookAt.transform.rotation;
     }
@@ -26,8 +29,6 @@ public class GreenFactController : MonoBehaviour {
             infoCanvas.GetComponent<RectTransform>().anchoredPosition = new Vector3(0.0f, 0.0f);//gameObject.transform.position.x, gameObject.transform.position.y + 10.0f);//gameObject.transform.position + displacement;
             infoCanvas.transform.rotation = cameraToLookAt.transform.rotation;
             infoCanvas.SetActive(true);
-            Debug.Log("beef: " + gameObject.transform.position);
-            Debug.Log("canvas: " + infoCanvas.GetComponent<RectTransform>().anchoredPosition);
         }
     }
 
