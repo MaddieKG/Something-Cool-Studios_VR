a03a07e127cdedf37fa3ecd3be5485d9dee9772c
Andrea Horvath <ahorvath@scu.edu>
2019-05-30 10:53:51 -0700

    added initialization for points

---
+++ unity game/SCS_vr/Assets/Scripts/PointsInitialization.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointsInitialization : MonoBehaviour {
+
+    // Use this for initialization
+    public PointsData pointsData;
+	void Start () {
+        pointsData.money = 15;
+        pointsData.green = 50;
+        pointsData.popularity = 50;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
